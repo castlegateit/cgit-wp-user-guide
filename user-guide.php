35b2897158134730ba2dcb99eff60847b296dd99
John Hughes <john@castlegateit.co.uk>
2015-08-06 10:56:37 +0100

    Make default guide aware of Admin Tweaks

--- user-guide.php
+++ user-guide.php
@@ -1,18 +1,18 @@
 <h3>The Dashboard</h3>
 
-<p>The main WordPress admin screen is the Dashboard. At the top of the screen is the admin bar, which contains quick links to the front end of the site and to your user profile. It also includes a &ldquo;New&rdquo; link, which provides a quick way of writing a new post or page. Depending on your WordPress theme and your user settings, the admin bar will also be displayed on the front end of the site while you are logged in.</p>
+<p>The main WordPress admin screen is the Dashboard. At the top of the screen is the admin bar, which contains quick links to the front end of the site and to your user profile. It also includes a &ldquo;New&rdquo; link, which provides a quick way of writing a new post or page.<?php if (!get_option('cgit_admin_hide_toolbar')): ?> The admin bar will also be displayed on the front end of the site while you are logged in.<?php endif; ?></p>
 
 <p>On the left side of the Dashboard is the main menu, which will contain the following items by default:</p>
 
 <ul>
     <li>Dashboard: the main WordPress admin screen</li>
-    <li>Posts: date-based news or blog posts, with optional categories and tags</li>
-    <li>Media: images and other files uploaded to the website</li>
-    <li>Pages: static content pages</li>
-    <li>Comments: comments submitted to posts and pages</li>
+    <?php if (!get_option('cgit_admin_hide_menu_posts')): ?><li>Posts: date-based news or blog posts, with optional categories and tags</li><?php endif; ?>
+    <?php if (!get_option('cgit_admin_hide_menu_media')): ?><li>Media: images and other files uploaded to the website</li><?php endif; ?>
+    <?php if (!get_option('cgit_admin_hide_menu_pages')): ?><li>Pages: static content pages</li><?php endif; ?>
+    <?php if (!get_option('cgit_admin_hide_menu_comments')): ?><li>Comments: comments submitted to posts and pages</li><?php endif; ?>
 </ul>
 
-<p>A customized WordPress theme may remove some of these entries and it may add others for other types of content more relevant to your site. When you click on these items, you will see a sub-menu. For example, the Posts menu can contain sub-menu items for Categories and Tags.</p>
+<p>A customized WordPress installation may add others items to this list for other types of content more relevant to your site. When you click on these items, you might see a sub-menu. For example, the Posts menu can contain sub-menu items for Categories and Tags.</p>
 
 <h3>Posts and Pages</h3>
 
@@ -34,7 +34,7 @@
 
 <h4>Categories and Tags</h4>
 
-<p>Posts can be organized using categories and tags. These appear on the right side of the edit screen. Categories are required, so any posts that are not assigned to a category will be placed in the &ldquo;Uncategorized&rdquo; category. Tags are entirely optional.</p>
+<p>Posts can be organized using categories and/or tags. These appear on the right side of the edit screen. Categories are required, so any posts that are not assigned to a category will be placed in the &ldquo;Uncategorized&rdquo; category. Tags are entirely optional.</p>
 
 <h4>Page Attributes</h4>
 
