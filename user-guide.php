319645fad09dad7f77db8e36972455dfec66b712
John Hughes <john@castlegateit.co.uk>
2015-08-06 10:44:30 +0100

    Remove title from default content

--- user-guide.php
+++ user-guide.php
@@ -1,5 +1,3 @@
-<h2>User Guide</h2>
-
 <h3>The Dashboard</h3>
 
 <p>The main WordPress admin screen is the Dashboard. At the top of the screen is the admin bar, which contains quick links to the front end of the site and to your user profile. It also includes a &ldquo;New&rdquo; link, which provides a quick way of writing a new post or page. Depending on your WordPress theme and your user settings, the admin bar will also be displayed on the front end of the site while you are logged in.</p>
@@ -7,11 +5,11 @@
 <p>On the left side of the Dashboard is the main menu, which will contain the following items by default:</p>
 
 <ul>
-  <li>Dashboard: the main WordPress admin screen</li>
-  <li>Posts: date-based news or blog posts, with optional categories and tags</li>
-  <li>Media: images and other files uploaded to the website</li>
-  <li>Pages: static content pages</li>
-  <li>Comments: comments submitted to posts and pages</li>
+    <li>Dashboard: the main WordPress admin screen</li>
+    <li>Posts: date-based news or blog posts, with optional categories and tags</li>
+    <li>Media: images and other files uploaded to the website</li>
+    <li>Pages: static content pages</li>
+    <li>Comments: comments submitted to posts and pages</li>
 </ul>
 
 <p>A customized WordPress theme may remove some of these entries and it may add others for other types of content more relevant to your site. When you click on these items, you will see a sub-menu. For example, the Posts menu can contain sub-menu items for Categories and Tags.</p>
@@ -28,7 +26,7 @@
 
 <h4>Content Editor</h4>
 
-<p>Below the title and permalink is the main content editor. This has a basic WYSIWYG interface that lets you mark text as bold or italic, enter bullet and numbered lists, and mark text as a heading. Please take care to use headings as headings, rather than using bold or other “presentational” formatting. Using the correct markup for a page makes your content more readable to Google and other search engines.</p>
+<p>Below the title and permalink is the main content editor. This has a basic WYSIWYG interface that lets you mark text as bold or italic, enter bullet and numbered lists, and mark text as a heading. Please take care to use headings as headings, rather than using bold or other &ldquo;presentational&rdquo; formatting. Using the correct markup for a page makes your content more readable to Google and other search engines.</p>
 
 <p>The button on the right of the content editor toolbar is labelled &ldquo;Toolbar Toggle&rdquo;. Use this to show more content formatting options. This also reveals a &ldquo;Paste as text&rdquo; button that allows you to paste formatted content from another application while stripping out any styles that would affect its appearance on your website. It is strongly recommended that you use this when pasting from a word processor, such as Microsoft Word.</p>
 
