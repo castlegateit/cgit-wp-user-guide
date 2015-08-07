3d3dd7abb9ddf24a2acecac8050c0ffdeb4e76c6
John Hughes <john@castlegateit.co.uk>
2015-08-07 10:58:42 +0100

    Add images

--- user-guide.php
+++ user-guide.php
@@ -12,6 +12,11 @@
     <?php if (!get_option('cgit_admin_hide_menu_comments')): ?><li>Comments: comments submitted to posts and pages</li><?php endif; ?>
 </ul>
 
+<figure>
+    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/dashboard.png" alt="" />
+    <figcaption>A typical WordPress Dashboard, showing the admin bar at the top and the main menu on the left.</figcaption>
+</figure>
+
 <p>A customized WordPress installation may add others items to this list for other types of content more relevant to your site. When you click on these items, you might see a sub-menu. For example, the Posts menu can contain sub-menu items for Categories and Tags.</p>
 
 <h3>Posts and Pages</h3>
@@ -19,6 +24,11 @@
 <p>By default, WordPress supports posts and pages. Posts are news or blog articles, displayed in date order on the front end of your website. They may also be accessed by date-, category-, or tag-based archives. Pages represent static content. Pages can be organized into a simple nested structure, so that one &ldquo;parent&rdquo; page can contain one or more &ldquo;child&rdquo; pages.</p>
 
 <p>Clicking on Posts or Pages in the main menu will display of list of entries, with some basic information about each one. From here, clicking on individual post or page titles will open the edit screen. You can also click on &ldquo;Add New&rdquo; at the top of the list to write a new post or page, also using the edit screen.</p>
+
+<figure>
+    <img src="<?php echo plugin_dir_url(__FILE__);?>images/pages.png" alt="" />
+    <figcaption>The full list of pages. Click &ldquo;Add New&rdquo; to add a new page. Click on the page title to edit that page.</figcaption>
+</figure>
 
 <h3>Editing Content</h3>
 
@@ -30,14 +40,29 @@
 
 <p>The button on the right of the content editor toolbar is labelled &ldquo;Toolbar Toggle&rdquo;. Use this to show more content formatting options. This also reveals a &ldquo;Paste as text&rdquo; button that allows you to paste formatted content from another application while stripping out any styles that would affect its appearance on your website. It is strongly recommended that you use this when pasting from a word processor, such as Microsoft Word.</p>
 
+<figure>
+    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/editor.png" alt="" />
+    <figcaption>The content editor, showing the default toolbar.</figcaption>
+</figure>
+
 <p>Note that the content editor is not a word processor; it is a means of preparing HTML content for your website. The styles and formatting are controlled by a site-wide stylesheet and the content may be filtered before it is displayed on the front-end of the site. Do not attempt to use the content editor to style the content or to add white space to affect the layout—let the website do that for you!</p>
 
 <h4>Categories and Tags</h4>
 
 <p>Posts can be organized using categories and/or tags. These appear on the right side of the edit screen. Categories are required, so any posts that are not assigned to a category will be placed in the &ldquo;Uncategorized&rdquo; category. Tags are entirely optional.</p>
 
+<figure>
+    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/taxonomies.png" alt="" />
+    <figcaption>Categories and tags. All posts must be assigned to at least one category. Tags are entirely optional.</figcaption>
+</figure>
+
 <h4>Page Attributes</h4>
 
 <p>The &ldquo;Page Attributes&rdquo; box on the right side of the edit screen lets you organize your pages. A page can act as a &ldquo;parent&rdquo; page, containing one or more &ldquo;child&rdquo; pages. Use the &ldquo;Parent&rdquo; dropdown in the Page Attributes box to assign the current page to a parent.</p>
 
 <p>The Page Attributes box also includes a field for &ldquo;Order&rdquo;, which lets you specify the menu order of the pages. Lower numbered pages will appear at the top of menus; higher numbered pages will appear lower. This will not have an effect if you are using a custom menu, or if your theme generates a menu using a non-standard method.</p>
+
+<figure>
+    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/attributes.png" alt="" />
+    <figcaption>Page attributes, showing the Order field.</figcaption>
+</figure>
